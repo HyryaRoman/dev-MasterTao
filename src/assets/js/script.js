28f23a4a8553d6ba48d2ee762729e360a463903a
HyryaRoman <hyrya.roman@gmail.com>
2024-09-14 13:50:38 +0300

    demo js objects

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -15,3 +15,41 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+
+const dog = {
+  name: "Lucky",
+  age: 5,
+  color: "brown",
+  parents: {
+    mother: 'Flora',
+    father: 'Trace'
+  }
+};
+
+const bird = {
+  name: "Ori",
+  age: 2,
+  color: "Blue",
+  parents: {
+    mother: 'Polly',
+    father: 'Hector'
+  }
+};
+
+const fish = {
+  name: "Goby",
+  age: 1,
+  color: "blue",
+  parents: {
+    mother: 'Sasha',
+    father: 'Max'
+  }
+};
+
+const logPet = function(pet) {
+  console.log("This is", pet.name, "and it's", pet.color, "and is", pet.age, "years old");
+}
+
+logPet(dog);
+logPet(bird);
+logPet(fish);
