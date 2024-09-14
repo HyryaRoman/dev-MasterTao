748eaf7488f2afbf9b5ae39490389df4da3a458b
HyryaRoman <hyrya.roman@gmail.com>
2024-09-14 14:03:49 +0300

    enable autoplay for banner

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,14 +1,15 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
+  autoplay: {
+    delay: 6000,
+  },
+
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
